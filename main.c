cf967245b275436aec24a5816b7a8cd714b3c9ba
MartimJales <you@example.com>
2023-02-07 01:26:42 +0000

    Logic of pipex done

--- main.c
+++ main.c
@@ -6,45 +6,63 @@
 /*   By: mjales <mjales@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 15:41:40 by mjales            #+#    #+#             */
-/*   Updated: 2023/02/06 17:38:07 by mjales           ###   ########.fr       */
+/*   Updated: 2023/02/07 00:49:13 by mjales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char *argv[], char *envp[])
 {
 	int		i;
 	char	**teste;
+	// char	*path;
+	int		infile;
+	int		outfile;
+	int		pid[2];
 
-	i = 0;
-	while (i++ < argc - 1)
+	(void)argc;
+	(void)envp;
+	(void)argv;
+	if (pipe(pid) == -1)
 	{
-		printf("%s\n", argv[i]);
+		printf("Error no pipe\n");
+		return (1);
 	}
-	printf("hello world\n");
+	// for(int i = 0; envp[i]; i++)
+	// {
+	// 	path = ft_strnstr(envp[i], "PATH=/", ft_strlen(envp[i]));
+	// 	if (path)
+	// 		break ;
+	// }
+	// path = path +5;
 	i = fork();
+	if (i == -1)
+	{
+		printf("Erro no fork\n");
+		return (2);
+	}
 	if (!i)
 	{
-		printf("===hello from child===\n");
+		close(pid[0]);
+		infile = open(argv[1], O_RDONLY);
+		dup2(infile, 0);
+		dup2(pid[1], 1);
 		argv[2] = ft_strjoin("/bin/", argv[2]);
-		printf("%s\n", argv[2]);
 		teste = ft_split(argv[2], ' ');
-		i = -1;
-		while(teste[++i])
-			printf("teste[%d] = %s\n", i, teste[i]);
 		execve(teste[0], teste, NULL);
-		argv[3] = ft_strjoin("/bin/", argv[3]);
-		printf("%s\n", argv[3]);
-		teste = ft_split(argv[3], ' ');
-		execve(argv[3], teste, NULL);
-		return (0);
 	}
 	else
 	{
 		wait(NULL);
+		close(pid[1]);
+		outfile = open(argv[4], O_WRONLY);
+		dup2(pid[0], 0);
+		dup2(outfile, 1);
+		argv[3] = ft_strjoin("/bin/", argv[3]);
+		teste = ft_split(argv[3], ' ');
+		execve(teste[0], teste, NULL);
 	}
-
 	printf("hellof from dad\n");
 
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: mjales <mjales@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 15:42:09 by mjales            #+#    #+#             */
-/*   Updated: 2023/02/06 15:54:53 by mjales           ###   ########.fr       */
+/*   Updated: 2023/02/07 00:24:27 by mjales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 # include <unistd.h>
 # include <limits.h>
 # include <sys/wait.h>
+# include <fcntl.h>
 
 #endif
